5b53302293a781744e4eae66190dde464f4e0f67
YS-GIT <epcp0627@gmail.com>
2026-03-01 12:37:05 +0900

    Update quit_smoking_bot.py

--- quit_smoking_bot.py
+++ quit_smoking_bot.py
@@ -12,11 +12,12 @@ def send_tweet():
 
     client = tweepy.Client(**auth_data)
 
-    # 오늘(2026-03-01) 기준 192일차가 나오도록 시작일 수정
+    # 2026-03-01 기준 192일차가 되려면 시작일은 2025-08-21입니다.
     start_date = datetime(2025, 8, 21, tzinfo=timezone(timedelta(hours=9)))
     now_kst = datetime.now(timezone(timedelta(hours=9)))
     days_passed = (now_kst - start_date).days
     
+    # 마지막 트윗 ID 읽기
     with open('last_tweet_id.txt', 'r') as f:
         last_id = f.read().strip()
 
@@ -24,7 +25,7 @@ def send_tweet():
     text = f"금연 {days_passed}일차"
     response = client.create_tweet(text=text, in_reply_to_tweet_id=last_id)
     
-    # 새로운 ID 저장
+    # 성공 시 새로운 트윗 ID 저장
     with open('last_tweet_id.txt', 'w') as f:
         f.write(str(response.data['id']))
     
